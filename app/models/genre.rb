39dea0c13252915b80dc9ba88342a490c31420fb
EC2 Default User <ec2-user@ip-172-31-6-105.ap-northeast-1.compute.internal>
2025-01-20 12:10:10 +0000

    add unique-genre-validation

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,9 @@ class Genre < ApplicationRecord
   has_many :recipes, through: :recipe_genres
   
   
-  validates :name, presence: true
+  validates :name, presence: true, 
+                uniqueness: { message: 'はすでに存在します' },
+                length: { maximum: 10 }
 
   def self.ransackable_attributes(auth_object = nil)
     ["name"]
